823bf4be59483ef4337495c91967ee1c0d83fcda
Lithiuman <76222494+Lithiuman@users.noreply.github.com>
2023-01-28 19:28:29 -0600

    remove arm

--- src/main/java/frc/robot/subsystems/pinkarm/Arm.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.subsystems.pinkarm;
-
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import frc.lib.team254.drivers.LazyTalonFX;
-
-public class Arm {
-}
--- src/main/java/frc/robot/subsystems/pinkarm/ArmIOSim.java
+++
@@ -1,4 +0,0 @@
-package frc.robot.subsystems.pinkarm;
-
-public class ArmIOSim {
-}
