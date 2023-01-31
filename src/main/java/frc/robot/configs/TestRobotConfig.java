394a53ac685bc06a841e3f5176fa6e220c427b94
Programmers <broncobots@gmail.com>
2023-01-31 17:09:42 -0600

    Implement getClawMotorID() in TestRobotConfig

--- src/main/java/frc/robot/configs/TestRobotConfig.java
+++ src/main/java/frc/robot/configs/TestRobotConfig.java
@@ -94,6 +94,13 @@ public class TestRobotConfig extends RobotConfig {
   private static final double AUTO_TURN_I_CONTROLLER = 0.0;
   private static final double AUTO_TURN_D_CONTROLLER = 0.0;
 
+  private static final int CLAW_MOTOR_ID = 9; // FIXME replace with correct id value
+
+  @Override
+  public int getClawMotorID() {
+    return CLAW_MOTOR_ID;
+  }
+
   @Override
   public double getSwerveAngleKP() {
     return ANGLE_KP;
