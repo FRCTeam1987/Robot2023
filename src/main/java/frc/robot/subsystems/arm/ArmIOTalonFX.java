c56d9f37bd51d242d7f63aafabf667213825225d
Programmers <broncobots@gmail.com>
2023-03-07 20:07:13 -0600

    Revert "Safety Changes"
    This reverts commit afbe1a285269b968bfdf8b1310669591d72b120a.

--- src/main/java/frc/robot/subsystems/arm/ArmIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIOTalonFX.java
@@ -210,13 +210,12 @@ public class ArmIOTalonFX implements ArmIO {
 
   @Override
   public void setArmLength(double inches) {
-    if (MINIMUM_EXTENSION_LENGTH_INCHES < inches && inches < ((MAX_HORIZONTAL_EXTENSION_INCHES / Math.cos(this.getArmAngle())) - CENTER_TO_EXTENSION) - CLAW_HEIGHT_INCHES) {
+    if (MINIMUM_EXTENSION_LENGTH_INCHES < inches && inches < MAXIMUM_EXTENSION_LENGTH_INCHES) {
       EXTENSION_TALON.set(
           TalonFXControlMode.MotionMagic,
           convertInchesToTicks(inches),
           DemandType.ArbitraryFeedForward,
-          (extensionArbitraryFeedforwardValues.get(getArmLength()))
-              * Math.sin(Math.toRadians(90.0 - getArmAngle())));
+          0.095 * Math.sin(Math.toRadians(90.0 - getArmAngle())));
     } else {
       armOverExtendAlert.set(true);
     }
