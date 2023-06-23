e21db1623f61d5de1bd8575897aba4053f537f77
Jonathan Smith <jhsmith0528@gmail.com>
2023-06-23 11:53:58 -0500

    Add some comments for `DriveToPiece` implementation.

--- src/main/java/frc/robot/commands/auto/DriveToPiece.java
+++ src/main/java/frc/robot/commands/auto/DriveToPiece.java
@@ -16,7 +16,7 @@ public class DriveToPiece extends CommandBase {
 
   private final Drivetrain drivetrain;
 
-  private String limelight = "limelight-fr";
+  private String limelight = "limelight-fr";  // TODO rename this default so that it makes sense for its new purpose OR use the parameter value
   private double initialXPose;
   private double initialYPose;
   private static final double kP = 0.03; // PID proportional gain
@@ -24,13 +24,14 @@ public class DriveToPiece extends CommandBase {
   private static final double kD = 0.01; // PID derivative gain
   private static final double kToleranceDegrees = 2.0; // Tolerance for reaching the desired angle
 
-  private final PIDController pidController;
+  private final PIDController pidController;  // TODO rename this - what does this pid controller for?
 
   public DriveToPiece(
       final Drivetrain drivetrain, final DoubleSupplier velocitySupplier, String limelight) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.velocitySupplier = velocitySupplier;
     this.drivetrain = drivetrain;
+    // TODO if using a parameter, then save the limelight string parameter to this.limelight
 
     // Create the PID controller
     pidController = new PIDController(kP, kI, kD);
@@ -45,16 +46,31 @@ public class DriveToPiece extends CommandBase {
     // Apply the output to the swerve
     this.initialXPose = drivetrain.getPoseX();
     this.initialYPose = drivetrain.getPoseY();
+    // TODO reset the PID controller
   }
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    /*
+     * TODO
+     * I like the practice of short circuiting to keep the code more readable. For example:
+     * if (isVisible() == false) {
+     *  return;
+     * }
+     * // do something if visible
+     */
     if (LimelightHelpers.getTV(limelight)) {
-      double output =
+      double output = // TODO name this so that it is clear what the output is used for.
           pidController.calculate(
               drivetrain.getRotation().getDegrees(),
               drivetrain.getRotation().getDegrees() - LimelightHelpers.getTX(limelight));
 
+      /*
+       * TODO
+       * If choosing to do field-relative, then velocity for x and y need to be separate values. Otherwise, field relative will drive in a diagonal.
+       * If making it robot-relative, then a single velocity should be fine. Just enable / disable field relative as needed.
+       * We may need to flex this between the two modes for auto vs teleop.
+       */
       final double velocity = velocitySupplier.getAsDouble();
       drivetrain.drive(velocity, velocity, output, true);
     }
@@ -63,18 +79,20 @@ public class DriveToPiece extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    if (distanceTraveled() > 1) {
-      drivetrain.stop();
+    if (distanceTraveled() > 1) { // TODO reuse this logic
+      drivetrain.stop();  // TODO do we want to stop at the end anyways or especially when interruped? since this is for collecting game pieces. prevent further auto steps?
     }
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return distanceTraveled() > 1;
+    return distanceTraveled() > 1;  // TODO make this 1 a constant or parameter / should this command run forever and let other commands stop it in a ParallelRaceGroup / teleop?
   }
 
   private double distanceTraveled() {
+    // TODO refactor this to use built in Pose logic / make reusable
+    // ex. Translation2d's getDistance()
     return Math.sqrt(
         Math.pow((drivetrain.getPoseY() - initialYPose), 2)
             + Math.pow((drivetrain.getPoseX() - initialXPose), 2));
